9e808143f8d2307ea225f3d5bda98c50d4ee1f7e
kvijayananth <kvananth11@gmail.com>
2020-11-21 15:37:17 +0530

    Authentication worked

--- src/app/service/userdata.service.ts
+++ src/app/service/userdata.service.ts
@@ -1,9 +1,17 @@
 import { Injectable } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/auth';
+import firebase from 'firebase/app';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserdataService {
 
-  constructor() { }
+  constructor(public auth: AngularFireAuth){}
+  login() {
+    return this.auth.signInWithPopup( new (firebase.auth as any).GoogleAuthProvider());
+  }
+  logout() {
+    return this.auth.signOut();
+  }
 }
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,13 +1,13 @@
 export const environment = {
   production: true,
   firebaseConfig : {
-    apiKey: "AIzaSyCpTdsgbweOB2Vd-1U86NGBw4BAZZr6-Fc",
-    authDomain: "verifytesttool-fcc96.firebaseapp.com",
-    databaseURL: "https://verifytesttool-fcc96.firebaseio.com",
-    projectId: "verifytesttool-fcc96",
-    storageBucket: "verifytesttool-fcc96.appspot.com",
-    messagingSenderId: "335626216615",
-    appId: "1:335626216615:web:65ffcf002ec9d0455ca782",
-    measurementId: "G-1CKK9ESXJ9"
+    apiKey: "AIzaSyBg-hLuMoJ_QinJQ2MyUHWJ8-ih7nINCrY",
+    authDomain: "verifytesttool.firebaseapp.com",
+    databaseURL: "https://verifytesttool.firebaseio.com",
+    projectId: "verifytesttool",
+    storageBucket: "verifytesttool.appspot.com",
+    messagingSenderId: "1075525420003",
+    appId: "1:1075525420003:web:427b9e01e93b350fa356d6",
+    measurementId: "G-39E01V32FM"
   }
 };
