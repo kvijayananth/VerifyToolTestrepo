f2224eefa1c403ce43b7d3cfa3d6f6f5f0b14b4c
kvijayananth <kvananth11@gmail.com>
2020-11-23 12:10:35 +0530

    Initial Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,7 @@ import { UserdataService } from './service/userdata.service';
 import { AngularFireAuth } from '@angular/fire/auth';
 import { Subscription } from 'rxjs';
 import { OnDestroy } from '@angular/core';
+import { take } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
@@ -12,17 +13,32 @@ import { OnDestroy } from '@angular/core';
 export class AppComponent implements OnDestroy {
   loggedin = false;
   subAuth: Subscription;
+  myarraydisplay: [] = [];
 
   constructor(public afAuth: AngularFireAuth, public tutorialService: UserdataService) {
 
     this.subAuth = this.afAuth.authState.subscribe(res => {
       if (res && res.uid) {
         this.loggedin = true;
-      } else {
-        this.loggedin = false;
+        this.tutorialService.getDocumentData('TestAngular', 'TestMain', 'TestSub').pipe(take(1)).subscribe(some => 
+          {
+          if (some && some.data()) {
+            for (const misson in some.data().Item) {//change to item for Manoj
+              console.log(misson, some.data().item[misson]);
+            }
+            this.myarraydisplay = some.data().item;
+          } 
+          
+          else {
+            console.log('Nodata found');
+          }
+        });
       }
-    });
-  }
+        else {
+          this.loggedin = false;
+        }
+  });
+}
   ngOnDestroy() {
     this.subAuth.unsubscribe();
   }
