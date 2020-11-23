421ec861e6d9c3c8d2263793afb84ea63769d345
kvijayananth <kvananth11@gmail.com>
2020-11-23 15:22:08 +0530

    Error in AppComponent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,21 +3,30 @@ import { UserdataService } from './service/userdata.service';
 import { AngularFireAuth } from '@angular/fire/auth';
 import { Subscription } from 'rxjs';
 import { OnDestroy } from '@angular/core';
+import { Observable } from 'rxjs';
+import { take }from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnDestroy {
+
+export class AppComponent  {
+  title(title: any) {
+    throw new Error('Method not implemented.');
+  }
   loggedin = false;
   subAuth: Subscription;
+  myarraydisplay:[]=[];
+  myitemsdisplay: Observable<any> | undefined
 
   constructor(public afAuth: AngularFireAuth, public tutorialService: UserdataService) {
 
     this.subAuth = this.afAuth.authState.subscribe(res => {
       if (res && res.uid) {
         this.loggedin = true;
+        this.myitemsdisplay=this.tutorialService.getDocumentData('TestAngular','TestMain', 'TestSub').pipe(take(1));
       } else {
         this.loggedin = false;
       }
--- src/app/service/userdata.service.ts
+++ src/app/service/userdata.service.ts
@@ -1,17 +1,27 @@
 import { Injectable } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/auth';
 import firebase from 'firebase/app';
+import { map } from 'rxjs/operators';
+import { AngularFirestore } from '@angular/fire/firestore';
+import {collectionData,doc} from 'rxfire/firestore';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserdataService {
 
-  constructor(public auth: AngularFireAuth){}
+  constructor(public auth: AngularFireAuth,  private db: AngularFirestore) { }
   login() {
-    return this.auth.signInWithPopup( new (firebase.auth as any).GoogleAuthProvider());
+    return this.auth.signInWithPopup(new (firebase.auth as any).GoogleAuthProvider());
   }
   logout() {
     return this.auth.signOut();
   }
+  getDocumentData(projectname: string, mainfield: string, subfield: string): any {
+    const collectionPath = projectname + '/' + mainfield + '/TestItems/' + subfield;  //items for Manoj
+    const davidDocRef = this.db.firestore.doc(collectionPath);
+    return doc(davidDocRef).pipe(
+      map(changes => ({ ...changes.data() })
+      ));
+    }
 }
