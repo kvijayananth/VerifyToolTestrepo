449f064ed8700b450aa7889ae6dc5f8099ad8d33
kvijayananth <kvananth11@gmail.com>
2020-11-23 14:15:26 +0530

    property items does not exist on type '[] '

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,7 @@ import { Subscription } from 'rxjs';
 import { OnDestroy } from '@angular/core';
 import { take } from 'rxjs/operators';
 
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -20,13 +21,13 @@ export class AppComponent implements OnDestroy {
     this.subAuth = this.afAuth.authState.subscribe(res => {
       if (res && res.uid) {
         this.loggedin = true;
-        this.tutorialService.getDocumentData('TestAngular', 'TestMain', 'TestSub').pipe(take(1)).subscribe(some => 
+        this.tutorialService.getDocumentData('TestAngular', 'TestMain', 'TestSub').pipe(take(1)).subscribe((some) => 
           {
           if (some && some.data()) {
-            for (const misson in some.data().Item) {//change to item for Manoj
-              console.log(misson, some.data().item[misson]);
+            for (const misson in some.data().Items) {//change to item for Manoj
+              console.log(misson, some.data().Items[misson]);
             }
-            this.myarraydisplay = some.data().item;
+            this.myarraydisplay = some.data().Items;
           } 
           
           else {
